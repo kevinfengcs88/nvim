00dd5d1400d0e6d43e17dbf1c0d41b684c31653b
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-04-05 13:24:44 -0400

    Added silent and noremap options to existing keymaps

--- lua/v9/keymaps.lua
+++ lua/v9/keymaps.lua
@@ -1,7 +1,7 @@
 local set = vim.opt
-
 local global = vim.g
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 global.mapleader = ';'
 global.maplocalleader = ';'
@@ -20,24 +20,26 @@ set.shiftround = true
 set.expandtab = true
 
 -- useful keymaps
-keymap.set('n', '<leader>w', ':wa<CR>')
-keymap.set('n', '<leader>q', ':qa<CR>')
-keymap.set('n', '<leader>m', ':MarkdownPreview<CR>')
-keymap.set('n', '<leader>s', ':source %<CR>')
-keymap.set('n', '<leader>p', ':PackerSync<CR>')
-keymap.set('n', '<leader>h', ':noh<CR>')
-keymap.set('n', '<leader>a', ':Alpha<CR>')
+keymap.set('n', '<leader>w', ':wa<CR>', opts)
+keymap.set('n', '<leader>q', ':qa<CR>', opts)
+keymap.set('n', '<leader>m', ':MarkdownPreview<CR>', opts)
+keymap.set('n', '<leader>s', ':source %<CR>', opts)
+keymap.set('n', '<leader>p', ':PackerSync<CR>', opts)
+keymap.set('n', '<leader>h', ':noh<CR>', opts)
+keymap.set('n', '<leader>a', ':Alpha<CR>', opts)
 
-keymap.set('n', '<C-a>', 'ggVG<CR>')
-keymap.set('n', '<TAB>', '>>')
-keymap.set('n', '<S-TAB>', '<<')
-keymap.set('n', '<CR>', 'o<Esc>') -- this creates a new line below the cursor and goes back into normal mode
-keymap.set('n', '<C-j>', '<C-w>w') -- makes toggling between buffers easier
+keymap.set('n', '<C-a>', 'ggVG<CR>', opts)
+keymap.set('n', '<TAB>', '>>', opts)
+keymap.set('n', '<S-TAB>', '<<', opts)
+-- this creates a new line below the cursor and goes back into normal mode
+keymap.set('n', '<CR>', 'o<Esc>', opts)
+-- makes toggling between buffers easier
+keymap.set('n', '<C-j>', '<C-w>w', opts)
 
 -- enables switching buffers from left to right and vice versa
-keymap.set('n', '<C-h>', '<C-w>h')
-keymap.set('n', '<C-l>', '<C-w>l')
+keymap.set('n', '<C-h>', '<C-w>h', opts)
+keymap.set('n', '<C-l>', '<C-w>l', opts)
 
-keymap.set('v', '<C-c>', '"+y')
-keymap.set('v', '<TAB>', '<S-.>')
-keymap.set('v', '<S-TAB>', '<S-,>')
+keymap.set('v', '<C-c>', '"+y', opts)
+keymap.set('v', '<TAB>', '<S-.>', opts)
+keymap.set('v', '<S-TAB>', '<S-,>', opts)
