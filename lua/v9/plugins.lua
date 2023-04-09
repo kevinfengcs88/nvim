f71d44928fd67f660ca7287ada26059bd6a0edc9
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-04-09 00:14:49 -0400

    Implemented temporary fix for improved startup time, some plugins may not work

--- lua/v9/plugins.lua
+++ lua/v9/plugins.lua
@@ -13,62 +13,64 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  'ellisonleao/gruvbox.nvim',
-  'nvim-tree/nvim-tree.lua',
-  'nvim-tree/nvim-web-devicons',
-  'nvim-lualine/lualine.nvim',
-  'marko-cerovac/material.nvim',
-  'nvim-treesitter/nvim-treesitter',
+  {'ellisonleao/gruvbox.nvim', lazy=true},
+  {'nvim-tree/nvim-tree.lua', lazy=true},
+  {'nvim-tree/nvim-web-devicons', lazy=true},
+  {'nvim-lualine/lualine.nvim', lazy=true},
+  {'marko-cerovac/material.nvim', lazy=true},
+  {'nvim-treesitter/nvim-treesitter', lazy=true},
   {
     'nvim-telescope/telescope.nvim',
     version = '0.1.0',
     dependencies = {
       'nvim-lua/plenary.nvim'
-    }
+    }, lazy=true
   },
   {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig'
+    'neovim/nvim-lspconfig',
+    lazy=true
   },
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  'rafamadriz/friendly-snippets',
+  {'hrsh7th/nvim-cmp', lazy=true},
+  {'hrsh7th/cmp-nvim-lsp', lazy=true},
+  {'L3MON4D3/LuaSnip', lazy=true},
+  {'saadparwaiz1/cmp_luasnip', lazy=true},
+  {'rafamadriz/friendly-snippets', lazy=true},
   { "akinsho/toggleterm.nvim",
     commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda",
-    config = "require('toggleterm')"
+    config = "require('toggleterm')",
+    lazy=true
   },
   {
       "iamcco/markdown-preview.nvim",
       config = function()
         vim.fn["mkdp#util#install"]()
-      end,
+      end, lazy=true
   },
   {
       'numToStr/Comment.nvim',
       config = function()
           require('Comment').setup()
-      end
+      end, lazy=true
   },
-  'JoosepAlviste/nvim-ts-context-commentstring',
+  {'JoosepAlviste/nvim-ts-context-commentstring', lazy=true},
   {
     "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
+      config = function() require("nvim-autopairs").setup {} end, lazy=true
   },
   {
     "windwp/nvim-ts-autotag",
-      config = function() require("nvim-ts-autotag").setup {} end
+      config = function() require("nvim-ts-autotag").setup {} end, lazy=true
   },
   {
       'goolord/alpha-nvim',
       config = function ()
           require'alpha'.setup(require'alpha.themes.dashboard'.config)
-      end
+      end, lazy=true
   },
-  'luk400/vim-lichess',
-  'dstein64/vim-startuptime'
+  {'luk400/vim-lichess'},
+  {'dstein64/vim-startuptime'}
 }
 
 local opts = {}
