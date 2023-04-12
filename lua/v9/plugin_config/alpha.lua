16b4ef375226600aeefb6f614d06e6738052fb3a
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-04-12 10:59:36 -0400

    Changed some alpha keymappings

--- lua/v9/plugin_config/alpha.lua
+++ lua/v9/plugin_config/alpha.lua
@@ -141,8 +141,8 @@ local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = header()
 
  dashboard.section.buttons.val = {
-   dashboard.button("CTRL+P", "  Find file", ":Telescope find_files <CR>"),
-   dashboard.button("f", "  New file", ":ene <CR>"),
+   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+   dashboard.button("e", "  New file", ":ene <CR>"),
    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
    dashboard.button("gr", "  Find text", ":Telescope live_grep <CR>"),
    dashboard.button("n", "  Open file tree", ":NvimTreeOpen <CR>"),
