7ee12bc1a5f4ef73352d0d7800f1e3f95901c518
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-04-15 16:48:44 -0400

    Added keymap for starting blackjack games

--- lua/v9/plugin_config/blackjack.lua
+++ lua/v9/plugin_config/blackjack.lua
@@ -1,5 +1,9 @@
+local opts = { noremap = true, silent = true }
+
 require("blackjack").setup({
   card_style = "mini", -- Can be "mini" or "large".
   suit_style = "black", -- Can be "black" or "white".
   scores_path = "/home/foo/blackjack_scores.json", -- Default location to store the scores.json file.
 })
+
+vim.keymap.set('n', '<leader>bj', ':BlackJackNewGame<CR>', opts)
