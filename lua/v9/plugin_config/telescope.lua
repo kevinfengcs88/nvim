16b426ec7423369b2a514288581910449939a819
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-04-20 17:19:01 -0400

    Telescope grep was not broken, ripgrep just wasn't installed locally on laptop

--- lua/v9/plugin_config/telescope.lua
+++ lua/v9/plugin_config/telescope.lua
@@ -6,7 +6,7 @@ end
 telescope.setup()
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fj', builtin.help_tags, {})
