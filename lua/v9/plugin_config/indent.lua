6f826d0dd8ed7aa81a3509f116c777fd3c914010
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-05-10 13:43:52 -0400

    Added indent line config with gray lines (actually required by init.lua duh)

--- lua/v9/plugin_config/indent.lua
+++ lua/v9/plugin_config/indent.lua
@@ -1,5 +1,10 @@
+vim.opt.termguicolors = true
+vim.opt.list = true
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#757575 gui=nocombine]]
+
 require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
+  char_highlight_list = {
+    'IndentBlanklineIndent1'
+  }
 }
