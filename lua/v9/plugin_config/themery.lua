36c1250e19bb11671faf5b2eac2969cd006c289b
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-05-10 16:23:40 -0400

    Added custome theme names for Themery

--- lua/v9/plugin_config/themery.lua
+++ lua/v9/plugin_config/themery.lua
@@ -1,5 +1,17 @@
 require('themery').setup({
-  themes = {'material', 'vscode', 'gruvbox-baby'},
+  -- themes = {'material', 'vscode', 'gruvbox-baby'},
+  themes = {{
+    name = 'material',
+    colorscheme = 'material'
+  },
+  {
+    name = 'vscode',
+    colorscheme = 'vscode'
+  },
+  {
+    name = 'gruvbox',
+    colorscheme = 'gruvbox-baby'
+  }},
   themeConfigFile = '~/.config/nvim/lua/v9/plugin_config/colorscheme.lua',
   livePreview = true
 })
