d22979358c85a0afe85830995f69837b7d332207
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-05-28 08:37:59 -0400

    Prevented completion menu from auto selecting

--- lua/v9/plugin_config/completions.lua
+++ lua/v9/plugin_config/completions.lua
@@ -11,7 +11,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-o>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
     snippet = {
         expand = function(args)
