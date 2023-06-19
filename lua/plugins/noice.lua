a415c776399fd3ad6bd216b551b3f24c53a9f57c
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-19 17:11:17 -0400

    Removed conflicting LSP settings from noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,6 +19,12 @@ return {
                     ['vim.lsp.util.stylize_markdown'] = true,
                     ['cmp.entry.get_documentation'] = true,
                 },
+                signature = {
+                    enabled = false
+                },
+                hover = {
+                    enabled = false
+                }
             },
             -- you can enable a preset for easier configuration
             presets = {
