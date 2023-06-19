00f8541569fcf605057c83fd98c58ee8dbe07277
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-19 13:18:14 -0400

    Added Comment.nvim

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,12 @@
+return {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function ()
+        local status_ok, comment = pcall(require, 'Comment')
+        if not status_ok then
+            return
+        end
+
+        comment.setup()
+    end
+}
