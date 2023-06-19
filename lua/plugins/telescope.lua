50a378f64dbb3f3e4de72767defe0b339edcfc7a
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-19 13:15:54 -0400

    Added telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+        local status_ok, telescope = pcall(require, 'telescope')
+        if not status_ok then
+            return
+        end
+
+        telescope.setup()
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
+        vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fj', builtin.help_tags, {})  
+    end
+}
