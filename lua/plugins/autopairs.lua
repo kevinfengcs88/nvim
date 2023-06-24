d3547e64d684e0d7e55dd9b820adbedfb7ce6148
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-24 16:56:36 -0400

    Fixed autotag plugin

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,27 +1,32 @@
 return {
-    { 
-    'windwp/nvim-autopairs',
-    event = { 'InsertEnter' },
-    config = function()
-        local autopairs_status_ok, autopairs = pcall(require, 'nvim-autopairs')
+    {
+        'windwp/nvim-autopairs',
+        event = { 'InsertEnter' },
+        config = function()
+            local autopairs_status_ok, autopairs = pcall(require, 'nvim-autopairs')
 
-        if not autopairs_status_ok then
-            return
+            if not autopairs_status_ok then
+                return
+            end
+
+            autopairs.setup({})
         end
-
-        autopairs.setup({})
-    end
     },
     {
-    'windwp/nvim-ts-autotag',
-    event = { 'InsertEnter' },
-    config = function()
-        local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
+        'windwp/nvim-ts-autotag',
+        event = { 'InsertEnter' },
 
-        if not autotag_status_ok then
-            return
-        end
-        autotag.setup({})
-    end
+        -- DO NOT USE A .setup() CALL
+        -- TREESITTER HANDLES THIS CONFIGURATION
+
+        -- config = function()
+        --     local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
+        --
+        --     if not autotag_status_ok then
+        --         return
+        --     end
+        --
+        --     autotag.setup({})
+        -- end
     }
 }
