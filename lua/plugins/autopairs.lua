348a3fd9f3e13b206ff7b3611125dd006ec56053
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-25 12:19:56 -0400

    Reverted autotag configuration to both treesitter and local setup function, more consistent(?)

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -16,17 +16,14 @@ return {
         'windwp/nvim-ts-autotag',
         event = { 'InsertEnter' },
 
-        -- DO NOT USE A .setup() CALL
-        -- TREESITTER HANDLES THIS CONFIGURATION
+        config = function()
+            local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
 
-        -- config = function()
-        --     local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
-        --
-        --     if not autotag_status_ok then
-        --         return
-        --     end
-        --
-        --     autotag.setup({})
-        -- end
+            if not autotag_status_ok then
+                return
+            end
+
+            autotag.setup({})
+        end
     }
 }
