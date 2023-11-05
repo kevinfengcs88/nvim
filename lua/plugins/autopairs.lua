626942a617bb7b05e77ca8fdf31a3266a9039eaf
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-11-05 16:35:13 -0500

    Fixed nvim-ts-autotag config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -42,16 +42,16 @@ return {
     },
     {
         'windwp/nvim-ts-autotag',
-        event = { 'InsertEnter' },
-
-        config = function()
-            local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
-
-            if not autotag_status_ok then
-                return
-            end
-
-            autotag.setup({})
-        end
+        -- event = { 'InsertEnter' },
+        --
+        -- config = function()
+        --     local autotag_status_ok, autotag = pcall(require, 'nvim-ts-autotag')
+        --
+        --     if not autotag_status_ok then
+        --         return
+        --     end
+        --
+        --     autotag.setup({})
+        -- end
     }
 }
