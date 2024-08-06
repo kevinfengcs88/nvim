7e300425fcd01aca4ceb19c90529a05fff4c9f14
Kevin Feng <kevinfeng.cs88@gmail.com>
2024-08-06 18:14:25 -0400

    markdown-preview is back (if shell cannot find npm, just run :call mkdp...)

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,26 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    main = "render-markdown",
-    opts = {},
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    {
+        'iamcco/markdown-preview.nvim',
+        build = 'cd app && npm install',
+        -- using npm to install rather than the vim function leads to significantly faster startup time
+        init = function()
+            -- how to get
+            -- https://github.com/iamcco/markdown-preview.nvim/issues/7
+            -- :call mkdp#util#install()
+            -- to run automatically on init
+            vim.g.mkdp_filetypes = { 'markdown' }
+        end,
+        config = function()
+            vim.keymap.set('n', '<leader>m', '<Plug>MarkdownPreviewToggle', { desc = 'Markdown Preview' })
+        end
+    },
+    {
+        'MeanderingProgrammer/markdown.nvim',
+        main = "render-markdown",
+        opts = {},
+        name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    }
 }
