267d940b1b1afb3cf8960b2c1e945a0163d61b70
Kevin Feng <kevinfeng.cs88@gmail.com>
2024-08-08 08:20:07 -0400

    README todo

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -14,13 +14,24 @@ return {
             vim.keymap.set('n', '<leader>m', '<Plug>MarkdownPreviewToggle', { desc = 'Markdown Preview' })
         end
     },
+    -- {
+    --     "OXY2DEV/markview.nvim",
+    --     lazy = false,      -- Recommended
+    --     -- ft = "markdown" -- If you decide to lazy-load anyway
+    --
+    --     dependencies = {
+    --         -- You will not need this if you installed the
+    --         -- parsers manually
+    --         -- Or if the parsers are in your $RUNTIMEPATH
+    --         "nvim-treesitter/nvim-treesitter",
+    --
+    --         "nvim-tree/nvim-web-devicons"
+    --     }
+    -- },
     {
         'MeanderingProgrammer/markdown.nvim',
         main = "render-markdown",
         opts = {},
         name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     }
 }
